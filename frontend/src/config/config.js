cdfe2a89bf899a90ce8a1a0b8b64b8e9f5ea80d7
Sulayman <developer.sulayman@gmail.com>
2025-03-26 18:25:22 +0600

    day 2

---
+++ frontend/src/config/config.js
@@ -0,0 +1,24 @@
+export const registerFormControls = [
+    {
+        name : "userName",
+        label: 'User Name',
+        placeholder: "Enter your user name",
+        componetType: "input",
+        type:"text",
+        
+    },
+    {
+        name : "password",
+        label: 'Password',
+        placeholder: "Enter your password",
+        componetType: "input",
+        type:"password"
+    },
+    {
+        name : "email",
+        label: 'Email',
+        placeholder: "Enter your Email",
+        componetType: "input",
+        type:"email"
+    },
+]
--- frontend/src/pages/authentication/Register.jsx
+++ frontend/src/pages/authentication/Register.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import Form from '@/components/common/Form'
+import { registerFormControls } from '@/config/config'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+const initailState ={
+  userName: '',
+  password: '',
+  email:''
+}
+function onSubmit(){
 
+}
 const Register = () => {
+  const [formData,setFormData] =useState(initailState)
   return (
-    <div>Register</div>
+    <div className='mx-auto w-full max-w-md space-y-6'>
+      <div className='text-center'>
+        <h1 className='text-3xl font-bold tracking-tight text-foreground'>Create a new account</h1>
+        <p>Already have an account</p>
+       <Link className='font-medium text-primary hover:underline' to="/auth/login">Login</Link>
+      </div>
+      <Form
+      formcontrols={registerFormControls}
+      buttonText={'Sign Up'}
+      formData={setFormData}
+      onSubmit={onSubmit}
+       />
+    </div>
   )
 }
 
